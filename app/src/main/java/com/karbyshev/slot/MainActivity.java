716694dbbabe5766f9d5bae7a5594bf4b35aa82a
Den Karbyshev <karbyshev.den@gmail.com>
2018-06-24 19:57:00 +0300

    Buttons listeners added

--- app/src/main/java/com/karbyshev/slot/MainActivity.java
+++ app/src/main/java/com/karbyshev/slot/MainActivity.java
@@ -5,28 +5,62 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mSettingsButton;
+    @BindView(R.id.settingsButton) Button mSettingsButton;
+    @BindView(R.id.minusButton) Button mMinusButton;
+    @BindView(R.id.plusButton) Button mPlusButton;
+    @BindView(R.id.spinButton) Button mSpinButton;
+
+    @BindView(R.id.jackpotTextView) TextView mJackpotTextView;
+    @BindView(R.id.myCoinsTextView) TextView mMyCoinsTextView;
+    @BindView(R.id.betTextView) TextView mBetTextView;
+
+    private long jackpot = 1000000;
+    private long myCoins = 995;
+    private long bet = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        mSettingsButton = (Button)findViewById(R.id.settingsButton);
-        mSettingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Settings!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        ButterKnife.bind(this);
+        mJackpotTextView.setText(String.valueOf(jackpot));
+        mMyCoinsTextView.setText(String.valueOf(myCoins));
+        mBetTextView.setText(String.valueOf(bet));
     }
 
-    public void spin(View view) {
-        Toast.makeText(this, "SPIN!", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.minusButton)
+    void minusBet(){
+        mPlusButton.setClickable(true);
+        if (bet > 5){
+            bet -= 5;
+            myCoins += 5;
+            mMyCoinsTextView.setText(String.valueOf(myCoins));
+            mBetTextView.setText(String.valueOf(bet));
+        } else {
+            mMinusButton.setClickable(false);
+        }
+    }
+
+    @OnClick(R.id.plusButton)
+    void plusBet(){
+        mMinusButton.setClickable(true);
+        if (bet < 100){
+            mPlusButton.setClickable(true);
+            bet += 5;
+            myCoins -= 5;
+            mMyCoinsTextView.setText(String.valueOf(myCoins));
+            mBetTextView.setText(String.valueOf(bet));
+        } else {
+            mPlusButton.setClickable(false);
+        }
     }
 }
